37db16115cfca58f595094e33dcfb4287e22c976
Zalts110 <zalts110@gmail.com>
2023-04-23 16:09:39 +0300

    hey

--- app/src/main/java/com/example/task1/MainActivity.java
+++ app/src/main/java/com/example/task1/MainActivity.java
@@ -5,6 +5,7 @@ import static android.view.View.VISIBLE;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 
 import android.content.Context;
@@ -20,6 +21,7 @@ import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.imageview.ShapeableImageView;
+import com.google.android.material.textview.MaterialTextView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -32,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
     private MaterialButton[] main_BTN_options;
     private ShapeableImageView[][] board;
 
-    private int numOfLanes = 3;
+    private int numOfLanes = 5;
     private CountDownTimer timerForGame;
 
     private int maxRow = 7;
@@ -46,6 +48,15 @@ public class MainActivity extends AppCompatActivity {
 
     private AlertDialog.Builder gameOverAlert;
 
+    private GameManager gManger;
+    private ShapeableImageView[][] coinBoard;
+
+    private MaterialTextView scoreBar;
+
+
+
+
+
 
 
     Random randomNumber;
@@ -56,7 +67,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         gameOverAlert = new AlertDialog.Builder(this);
         findViews();
-        startTime();
+        gManger = new GameManager(main_IMG_hearts,board,coinBoard,scoreBar,this.getApplicationContext());
+
     }
 
     private void findViews() {
@@ -68,157 +80,51 @@ public class MainActivity extends AppCompatActivity {
                 findViewById(R.id.main_FAB_rightbutton),
                 findViewById(R.id.main_FAB_leftbutton)
         };
+        scoreBar = findViewById(R.id.main_LBL_score);
+
         board = new ShapeableImageView[][]{
-                {findViewById(R.id.main_grid_0_0), findViewById(R.id.main_grid_0_1), findViewById(R.id.main_grid_0_2)},
-                {findViewById(R.id.main_grid_1_0), findViewById(R.id.main_grid_1_1), findViewById(R.id.main_grid_1_2)},
-                {findViewById(R.id.main_grid_2_0), findViewById(R.id.main_grid_2_1), findViewById(R.id.main_grid_2_2)},
-                {findViewById(R.id.main_grid_3_0), findViewById(R.id.main_grid_3_1), findViewById(R.id.main_grid_3_2)},
-                {findViewById(R.id.main_grid_4_0), findViewById(R.id.main_grid_4_1), findViewById(R.id.main_grid_4_2)},
-                {findViewById(R.id.main_grid_5_0), findViewById(R.id.main_grid_5_1), findViewById(R.id.main_grid_5_2)},
-                {findViewById(R.id.main_grid_6_0), findViewById(R.id.main_grid_6_1), findViewById(R.id.main_grid_6_2)},
-                {findViewById(R.id.main_grid_7_0), findViewById(R.id.main_grid_7_1), findViewById(R.id.main_grid_7_2)},
+                {findViewById(R.id.main_grid_0_0), findViewById(R.id.main_grid_0_1), findViewById(R.id.main_grid_0_2),findViewById(R.id.main_grid_0_3),findViewById(R.id.main_grid_0_4)},
+                {findViewById(R.id.main_grid_1_0), findViewById(R.id.main_grid_1_1), findViewById(R.id.main_grid_1_2),findViewById(R.id.main_grid_1_3),findViewById(R.id.main_grid_1_4)},
+                {findViewById(R.id.main_grid_2_0), findViewById(R.id.main_grid_2_1), findViewById(R.id.main_grid_2_2),findViewById(R.id.main_grid_2_3),findViewById(R.id.main_grid_2_4)},
+                {findViewById(R.id.main_grid_3_0), findViewById(R.id.main_grid_3_1), findViewById(R.id.main_grid_3_2),findViewById(R.id.main_grid_3_3),findViewById(R.id.main_grid_3_4)},
+                {findViewById(R.id.main_grid_4_0), findViewById(R.id.main_grid_4_1), findViewById(R.id.main_grid_4_2),findViewById(R.id.main_grid_4_3),findViewById(R.id.main_grid_4_4)},
+                {findViewById(R.id.main_grid_5_0), findViewById(R.id.main_grid_5_1), findViewById(R.id.main_grid_5_2),findViewById(R.id.main_grid_5_3),findViewById(R.id.main_grid_5_4)},
+                {findViewById(R.id.main_grid_6_0), findViewById(R.id.main_grid_6_1), findViewById(R.id.main_grid_6_2),findViewById(R.id.main_grid_6_3),findViewById(R.id.main_grid_6_4)},
+                {findViewById(R.id.main_grid_7_0), findViewById(R.id.main_grid_7_1), findViewById(R.id.main_grid_7_2),findViewById(R.id.main_grid_7_3),findViewById(R.id.main_grid_7_4)},
+        };
+
+        coinBoard = new ShapeableImageView[][]{
+                {findViewById(R.id.coin_0_0), findViewById(R.id.coin_0_1), findViewById(R.id.coin_0_2),findViewById(R.id.coin_0_3),findViewById(R.id.coin_0_4)},
+                {findViewById(R.id.coin_1_0), findViewById(R.id.coin_1_1), findViewById(R.id.coin_1_2),findViewById(R.id.coin_1_3),findViewById(R.id.coin_1_4)},
+                {findViewById(R.id.coin_2_0), findViewById(R.id.coin_2_1), findViewById(R.id.coin_2_2),findViewById(R.id.coin_2_3),findViewById(R.id.coin_2_4)},
+                {findViewById(R.id.coin_3_0), findViewById(R.id.coin_3_1), findViewById(R.id.coin_3_2),findViewById(R.id.coin_3_3),findViewById(R.id.coin_3_4)},
+                {findViewById(R.id.coin_4_0), findViewById(R.id.coin_4_1), findViewById(R.id.coin_4_2),findViewById(R.id.coin_4_3),findViewById(R.id.coin_4_4)},
+                {findViewById(R.id.coin_5_0), findViewById(R.id.coin_5_1), findViewById(R.id.coin_5_2),findViewById(R.id.coin_5_3),findViewById(R.id.coin_5_4)},
+                {findViewById(R.id.coin_6_0), findViewById(R.id.coin_6_1), findViewById(R.id.coin_6_2),findViewById(R.id.coin_6_3),findViewById(R.id.coin_6_4)},
+
         };
     }
 
 
 
     public void moveRight(View view) {
-        if (board[7][0].getVisibility() == VISIBLE) {
-            board[7][0].setVisibility(INVISIBLE);
-            board[7][1].setVisibility(VISIBLE);
-
-
-        } else if (board[7][1].getVisibility() == VISIBLE) {
-            board[7][1].setVisibility(INVISIBLE);
-            board[7][2].setVisibility(VISIBLE);
-
-        }
+       gManger.moveRight(view);
     }
 
     public void moveLeft(View view) {
-        if (board[7][2].getVisibility() == VISIBLE) {
-            board[7][2].setVisibility(INVISIBLE);
-            board[7][1].setVisibility(VISIBLE);
-
-
-        } else if (board[7][1].getVisibility() == VISIBLE) {
-            board[7][1].setVisibility(INVISIBLE);
-            board[7][0].setVisibility(VISIBLE);
-
-        }
+      gManger.moveLeft(view);
     }
 
-    public void randomRyno()
-    {
-        Random rand = new Random();
-        int randomNum = rand.nextInt(3);
-        if(!randomRock1)
-        {
-            board[0][randomNum].setVisibility(VISIBLE);
-            randomRock1 = true;
-        }else{
-            randomRock1 = false;
-        }
+
+
+
+
 
     }
 
-    public void moveRynosRowDown() {
-
-        for (int i = maxRow - 1; i > 0; i--)
-        {
-            for(int j = 0; j < 3; j++)
-            {
-                board[i][j].setVisibility(board[i - 1][j].getVisibility());
-            }
-        }
-
-        for(int i = 0; i < 3 ; i ++)
-        {
-            board[0][i].setVisibility(INVISIBLE);
-        }
-
-        lifeDown();
-    }
-
-    private void startTime() {
-        startTime = System.currentTimeMillis();
-        if (timerForGame == null) {
-            timerForGame = new CountDownTimer(999999999, DELAY) {
-                @Override
-                public void onTick(long millisUntilFinished) {
-                    moveRynosRowDown();
-                    randomRyno();
-                }
-
-                @Override
-                public void onFinish() {
-                    timerForGame.cancel();
-                }
-            }.start();
-
-        }
-    }
-
-    private void lifeDown()
-    {
-        if (mistakeCount > numberOfHearts)
-        {
-            gameOver();
-        }
 
 
-        for (int i = 0 ; i < 3; i++)
-        {
-            if((board[7][i].getVisibility() == VISIBLE) && (board[6][i].getVisibility() == VISIBLE))
-            {
-                main_IMG_hearts[numberOfHearts - mistakeCount].setVisibility(INVISIBLE);
-                mistakeCount++;
-                Toast.makeText(this,"Crash!",Toast.LENGTH_LONG).show();
-                Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    v.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE));
-                } else {
-                    //deprecated in API 26
-                    v.vibrate(500);
-                }
-            }
-        }
-    }
 
-private void gameOver()
-    {
-        stopTimer();
-        gameOverAlert.setMessage("press Again for playing another round");
-        gameOverAlert.setCancelable(false);
 
-        gameOverAlert.setPositiveButton("AGAIN", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) { initGame();
 
-            }
-        });
 
-        AlertDialog message = gameOverAlert.create();
-        message.setTitle("You Lost");
-        message.show();
-    }
-
-    private void initGame()
-    {
-        for (int i = 0; i < maxRow; i++)
-        {
-            for (int j = 0; j < 3; j++)
-            {
-                board[i][j].setVisibility(INVISIBLE);
-            }
-        }
-        for(int i = 0 ; i < 3; i++)
-        {
-            main_IMG_hearts[i].setVisibility(VISIBLE);
-        }
-        mistakeCount = 0;
-        timerForGame.start();
-    }
-
-    private void stopTimer(){timerForGame.cancel();}
-}
